b39b51e40d9495124b107ea3875d5f1663cb9dfa
Leonardo Rodrigues <116523494+leonardo7en@users.noreply.github.com>
2023-01-27 22:33:55 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,11 @@
+alert ("Bem vindo! A seguir pediremos que nos informes alguns dados.")
+
+let name = prompt ("Qual é o seu nome?")
+
+console.log(name)
+
+let age = prompt("Qual é a sua idade?")
+
+let ageconfirmation = confirm ("Sua idade é " + age + " anos.")
+
+alert("nome:  " + name + "\n| Idade: " + age + "\n| Idade confirmada: "+ ageconfirmation)
